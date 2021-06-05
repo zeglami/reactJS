2c48a9e0922d75871ce14058a1935f9f005b5c97
zeglami <zeglami.abdelhamid@gmail.com>
2021-06-05 13:39:08 +0100

    week3 done assign

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -52,7 +52,7 @@ function About(props) {
         <div className="col-12 col-md-6">
           <h2>Our History</h2>
           <p>
-            Started in 2010, Ristorante con Fusion quickly established itself as
+            Started in 2010, Ristorante con  Fusion quickly established itself as
             a culinary icon par excellence in Hong Kong. With its unique brand
             of world fusion cuisine that can be found nowhere else, it enjoys
             patronage from the A-list clientele in Hong Kong. Featuring four of
