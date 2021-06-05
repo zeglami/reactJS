76c0477f8566d26ddc1510cda30e00383e8fbd76
zeglami <zeglami.abdelhamid@gmail.com>
2021-06-05 13:37:43 +0100

    week done assign

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import Menu from './MenuComponent';
-import Contact from './ContactComponent';
-import { DISHES } from '../shared/dishes';
-import Header from './HeaderComponent'
-import Footer from './FooterComponent';
-import {Switch,Route,Redirect} from 'react-router-dom'
-import Home from './HomeComponent'
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
-import DishDetail from './DishdetailComponent';
-import About from './AboutComponent'
+import React, { Component } from "react";
+import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
+import { DISHES } from "../shared/dishes";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./HomeComponent";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+import DishDetail from "./DishdetailComponent";
+import About from "./AboutComponent";
 
 class Main extends Component {
-
   constructor(props) {
     super(props);
 
@@ -21,48 +20,56 @@ class Main extends Component {
       dishes: DISHES,
       comments: COMMENTS,
       promotions: PROMOTIONS,
-      leaders: LEADERS
+      leaders: LEADERS,
     };
   }
 
-
   render() {
-    
-    const DishWithId = ({match}) => {
-      return(
-          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
       );
     };
     const HomePage = () => {
-      return(
-          <Home 
-              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-          />
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
-    }
-    const AboutPage =()=>{
-      return(
-        <About leaders={this.state.leaders}/>
-      );
-    }
+    };
+    const AboutPage = () => {
+      return <About leaders={this.state.leaders} />;
+    };
 
     return (
       <div>
         <Header />
         <Switch>
-        <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact}  />
-             <Route exact path="/aboutus" component={AboutPage}/>
-              <Redirect to="/home" />
+          <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route
+              exact
+              path="/menu"
+              component={() => <Menu dishes={this.state.dishes} />}
+            />
+            <Route path="/menu/:dishId" component={DishWithId} />
+            <Route exact path="/contactus" component={Contact} />
+            <Route exact path="/aboutus" component={AboutPage} />
+            <Redirect to="/home" />
           </Switch>
         </Switch>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
